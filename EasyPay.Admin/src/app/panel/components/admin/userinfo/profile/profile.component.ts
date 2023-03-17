07b61939641b0664ee1bb04b8f6570de5a9daa5c
hafezabadian <hafezabadian@gmail.com>
2023-03-18 02:25:45 +0330

    two way binding input forms

---
+++ EasyPay.Admin/src/app/panel/components/admin/userinfo/profile/profile.component.ts
@@ -0,0 +1,33 @@
+import { User } from '../../../../../models/user';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import * as moment from 'jalali-moment';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user!: User;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser(){
+    this.route.data.subscribe(
+      data => {
+      this.user = data['userdata'];
+      }
+      )
+  }
+
+  genderstring(gen:boolean):string{
+    return gen? 'مرد' : 'زن';
+  }
+
+}
